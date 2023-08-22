d0fdcb6f3be9f12ec4aac2b7e809802ca5ba3c1d
Tyrell <tyrellkamara-oliver@pursuit.org>
2023-08-22 11:47:01 -0400

    pulling from the main

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,28 @@
+
+import React, { useState } from 'react';
+
+
+function SearchBar({ onSearch }) {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSearch(searchTerm); // Pass the search term to the parent component
+  };
+
+  return (
+    <div className="search-bar">
+      <form onSubmit={handleSubmit}>
+        <input className='searchBarArea'
+          type="text"
+          placeholder="Search for videos"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button className='searchBarArea-btn' type="submit">Search</button>
+      </form>
+    </div>
+  );
+}
+
+export default SearchBar;
