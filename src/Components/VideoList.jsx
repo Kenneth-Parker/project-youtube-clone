b87a2a07134d5faedfed4538e2efdc74cbe88549
Kenneth-Parker <kennethkavon@gmail.com>
2023-08-23 16:30:44 -0400

    update

--- src/Components/VideoList.jsx
+++ src/Components/VideoList.jsx
@@ -1,13 +1,18 @@
-// VideoList.jsx
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function VideoList({ videos }) {
   return (
     <div className="video-list">
       {videos.map((video) => (
         <div className="video-card" key={video.id.videoId}>
-          <img src={video.snippet.thumbnails.default.url} alt={video.snippet.title} />
-          <p>{video.snippet.title}</p>
+          <Link to={`/videos/${video.id.videoId}`}>
+            <div >
+              <img className='listCard' src={video.snippet.thumbnails.default.url} alt={video.snippet.title} />
+            <p>{video.snippet.title}</p>
+            <br />
+            </div>
+          </Link>
         </div>
       ))}
     </div>
