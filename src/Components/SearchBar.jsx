d1da5b8df0b812d20e8c6dac6fe1484d70b47e28
Tyrell <tyrellkamara-oliver@pursuit.org>
2023-08-24 11:57:55 -0400

    commit

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,37 +1,35 @@
-
-import React, { useState } from 'react';
-
+import React, { useState } from "react";
 
 function SearchBar({ onSearch }) {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [searchError, setSearchError]= useState('');
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchError, setSearchError] = useState("");
 
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        onSearch(searchTerm); // Pass the search term to the parent component
-    };
-
-
-    if(searchTerm === ''){
-        setSearchError('No Search Results Yet! Please submit a search above !')
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (searchTerm === "") {
+      setSearchError("No Search Results Yet! Please submit a search above !");
+    } else {
+      onSearch(searchTerm); // Pass the search term to the parent component
     }
+  };
 
-    return (
-        <div className="search-bar">
-            <form onSubmit={handleSubmit}>
-                <input className='searchBarArea'
-                    type="text"
-                    placeholder="Search for videos"
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
-                <button className='searchBarArea-btn' type="submit">Search</button>
-                {searchError && <p className="error-message">{searchError}</p>}
-            </form>
-        </div>
-    );
-    
+  return (
+    <div className="search-bar">
+      <form onSubmit={handleSubmit}>
+        <input
+          className="searchBarArea"
+          type="text"
+          placeholder="Search for videos"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button className="searchBarArea-btn" type="submit">
+          Search
+        </button>
+        {searchError && <p className="error-message">{searchError}</p>}
+      </form>
+    </div>
+  );
 }
 
 export default SearchBar;
--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { BrowserRouter as Router} from "react-router-dom"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Router>
+    {/* <Router> */}
     <App />
-    </Router>
+    {/* </Router> */}
   </React.StrictMode>,
 )
